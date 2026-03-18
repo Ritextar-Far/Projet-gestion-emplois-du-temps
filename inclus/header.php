885e4bc2d013bc53214e44f34dc7b363b7c98543
SamOnC0de <samlyceemail@gmail.com>
2026-03-18 15:35:25 +0100

    Update header.php

--- inclus/header.php
+++ inclus/header.php
@@ -8,7 +8,7 @@ $page_actuelle = basename($_SERVER['PHP_SELF']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion Licence - Lycée Saint-Vincent</title>
-    <link rel="stylesheet" href="header.css"> 
+    <link rel="stylesheet" href="../assets/css/header.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -16,7 +16,7 @@ $page_actuelle = basename($_SERVER['PHP_SELF']);
 <div class="page">
     <aside class="barre-gauche">
         <div class="header-logo">
-           <img src="Logo_StVincent.png" alt="Logo établissement" class="logo">
+           <img src="../assets/images/logo.png" alt="Logo établissement" class="logo">
             <div class="text-logo">
                 <p class="gros-text">Lycée Saint-Vincent</p>
                 <p class="sous-titre">Enseignement Supérieur</p>
@@ -27,30 +27,30 @@ $page_actuelle = basename($_SERVER['PHP_SELF']);
             <p class="titre-section">MENU</p>
             <ul>
                 <li class="<?= ($page_actuelle == 'calendrier.php') ? 'active' : '' ?>">
-                    <a href="calendrier.php"><img src="calendrier.png" class="icon-menu">Calendrier</a>
+                    <a href="calendrier.php"><img src="../assets/images/Vector.png" class="icon-menu">Calendrier</a>
                 </li>
                 <li class="<?= ($page_actuelle == 'interventions.php') ? 'active' : '' ?>">
                     <a href="interventions.php">
-                        <img src="Interventions.png" class="icon-menu">Interventions</a>
+                        <img src="../assets/images/typeintervention.png" class="icon-menu">Interventions</a>
                 </li>
-                <li class="<?= ($page_actuelle == 'enseignants.php') ? 'active' : '' ?>">
-                    <a href="enseignants.php"><img src="Corp_enseignant.png" class="icon-menu">Corps enseignant</a>
+                <li class="<?= ($page_actuelle == 'corps_enseignant.php') ? 'active' : '' ?>">
+                    <a href="corps_enseignant.php"><img src="../assets/images/Corp_enseignant.png" class="icon-menu">Corps enseignant</a>
                 </li>
             </ul>
 
             <p class="titre-section">PARAMÉTRAGE</p>
             <ul>
                 <li class="<?= ($page_actuelle == 'modules.php') ? 'active' : '' ?>">
-                    <a href="modules.php"><img src="Modules.png" class="icon-menu">Modules</a>
+                    <a href="modules.php"><img src="../assets/images/Modules.png" class="icon-menu">Modules</a>
                 </li>
-                <li class="<?= ($page_actuelle == 'types.php') ? 'active' : '' ?>">
-                    <a href="types.php"><img src="Interventions.png" class="icon-menu">Types d'intervention</a>
+                <li class="<?= ($page_actuelle == 'type_intervention.php') ? 'active' : '' ?>">
+                    <a href="type_intervention.php"><img src="../assets/images/typeintervention.png" class="icon-menu">Types d'intervention</a>
                 </li>
             </ul>
         </nav>
 
         <div class="profil-utilisateur">
-            <img src="Stella_Ribas-PDP.png" alt="Avatar" class="avatar">
+            <img src="../public/assets/images/Stella_Ribas-PDP.png" alt="Avatar" class="avatar">
             <div class="infos-utilisateur">
                 <p class="nom">Stella Ribas</p>
                 <p class="role">Administrateur</p>
