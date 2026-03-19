d4c4dadd83e2e2e561d2549faa35faa084f0e8f3
SamOnC0de <samlyceemail@gmail.com>
2026-03-19 16:58:22 +0100

    Update type_intervention.php

--- public/pages/type_intervention.php
+++ public/pages/type_intervention.php
@@ -6,25 +6,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Connexion au portail</title>
+    <link rel="stylesheet" href="../assets/css/header.css">
     <link rel="stylesheet" href="../assets/css/typeintervention.css">
     <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Quicksand:wght@300..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 </head>
 <body class="pageintervention">
     <div class="page">
-        <aside class="barre-gauche">
-            <img src="../assets/images/logo.png" alt="Logo établissement" class="logo">
-            <div class="text-logo">
-                <p class="gros-text">Lycée Saint-vincent</p>
-                <p class="sous-titre">Enseignement supérieur</p>
-            </div>
-        </aside>
+        <?php require_once '../../inclus/header.php'; ?>
         <main class="contenu">
-            <h1 class="titre-principal">Types d'intervention</h1>
+            <p class="titre-principal">Types d'intervention</p>
             <section class="bloc-filtre">
                 <h3 class="titre-filtre">Filtres</h3>
-
-
                 <form class="formulaire-filtre" method="POST" action="">
                     <div class="champ">
                         <label>Nom</label>
