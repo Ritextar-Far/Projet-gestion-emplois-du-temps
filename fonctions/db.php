c306a52b45bd9fcca9feee974f3b3b26bec9a034
SamOnC0de <samlyceemail@gmail.com>
2026-03-22 21:19:58 +0100

    Update db.php

--- fonctions/db.php
+++ fonctions/db.php
@@ -1,13 +1,12 @@
 <?php
-require_once('public/Inclus/config.php');
+require_once(dirname(__FILE__) . '/../inclus/config.php');
+// dirname(__FILE__) = en clair le dossier où db.php se trouve, peu importe qui l'appelle, comme ça le chemin il est pas "corrompu" ou cassé peu importe
 
 try {
     $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
-
     $pdo = new PDO($dsn, DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
