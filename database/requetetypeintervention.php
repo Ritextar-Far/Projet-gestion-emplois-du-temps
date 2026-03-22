b2b6761abd171113aa2bf73079c27dbb2cfb027d
SamOnC0de <samlyceemail@gmail.com>
2026-03-22 21:24:40 +0100

    Add files via upload

---
+++ database/requetetypeintervention.php
@@ -0,0 +1,14 @@
+<?php
+
+$filtre_nom = isset($_POST['nom']) ? trim($_POST['nom']) : '';
+
+if ($filtre_nom !== '') {
+    $stmt = $pdo->prepare("SELECT * FROM intervention_type WHERE name LIKE ? ORDER BY name ASC");
+    $stmt->execute(['%' . $filtre_nom . '%']);
+} else {
+    $stmt = $pdo->query("SELECT * FROM intervention_type ORDER BY name ASC");
+}
+
+$types = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$nb_types = count($types);
+?>
